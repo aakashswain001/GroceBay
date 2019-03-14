69f12620dc81e1fae47e25c253b5d7ab07d62ba9
Akash Ranjan Swain <aakashswain001@gmail.com>
2019-03-15 04:06:10 +0530

    Add logic to Profile fragment

--- app/src/main/java/com/grocebay/grocebay/ProfileFragment.java
+++ app/src/main/java/com/grocebay/grocebay/ProfileFragment.java
@@ -1,31 +1,59 @@
 package com.grocebay.grocebay;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.grocebay.grocebay.model.User;
+import com.grocebay.grocebay.utils.SharedPrefManager;
 
 
 public class ProfileFragment extends Fragment {
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
+    View mView;
+    TextView textViewId, textViewName, textViewEmail, textViewPhone;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        mView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        textViewId = mView.findViewById(R.id.textViewId);
+        textViewName = mView.findViewById(R.id.textViewUsername);
+        textViewEmail = mView.findViewById(R.id.textViewEmail);
+        textViewPhone = mView.findViewById(R.id.textViewPhone);
+
+
+        //getting the current user
+        User user = SharedPrefManager.getInstance(getContext()).getUser();
+
+        //setting the values to the textviews
+        textViewId.setText(String.valueOf(user.getId()));
+        textViewName.setText(user.getName());
+        textViewEmail.setText(user.getEmail());
+        textViewPhone.setText(user.getPhone());
+
+        //when the user presses logout button
+        //calling the logout method
+        mView.findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+                SharedPrefManager.getInstance(getContext()).logout();
+            }
+        });
+        return mView;
     }
 
-
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Profile");
+    }
 }
